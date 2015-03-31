d6981ece89f620083f07d5f6f0127b768689ce9f
shiqing881215 <shiqing881215@gmail.com>
2015-03-30 23:42:11 -0700

    Initial Commit

---
+++ CookieCrunch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CookieCrunch
+//
+//  Created by shi qing on 15/3/30.
+//  Copyright (c) 2015年 shi qing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CookieCrunch/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  CookieCrunch
+//
+
+//  Copyright (c) 2015年 shi qing. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
